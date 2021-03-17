26510c1e22108ee1f1c0009e18c199a6da3def42
Ross Beaudin <60157958+RoosterMcBooster@users.noreply.github.com>
2021-03-16 21:33:18 -0600

    Added side bar functionality
    Currently you can switch pages with the side bar.
    
    Note: There is a small bug with the pages shifting to the right with every change page. This is most likely caused by the styles.

--- MainPage.py
+++ MainPage.py
@@ -1,13 +1,16 @@
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pandas as pd
 import plotly.graph_objects as go
 
-
+# rem means relative font size.
 class MainPage:
-    app = dash.Dash()
-    colors = {'background': '#272727',
+
+    colors = {'background': '#202530',
               'text': '#ffffff'}
     data = pd.read_csv('./data/tv_shows.csv')
     data2 = data.groupby(data['Age'], as_index=False).size()
@@ -15,22 +18,50 @@ class MainPage:
     dropDownDict = []
     i = 0
 
+    sidebar_style = {
+        "position": "fixed",
+        "top": 0,
+        "left": 0,
+        "bottom": 0,
+        "width": "16rem",
+        "padding": "2rem 1rem",
+        "background-color": "#272D3F",
+    }
+
+    mainHomeStyle = {
+        "backgroundColor": colors['background'],
+        "margin-left": "10rem",
+        "margin-right": "2rem",
+        "padding": "2rem 1rem",
+    }
 
     def __init__(self):
         for i in range(len(self.ageGroup)):
             self.dropDownDict.append({'label': self.ageGroup[i], 'value': self.ageGroup[i]})
         # print(ageGroup)
         # print(data)
-        self.app.layout = html.Div(style={'backgroundColor': self.colors['background']}, children=[
+
+        self.sidebar = html.Div([
+            dbc.Nav([
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Analytics", href="/Analytics", active="exact")
+            ],
+                vertical=True,
+                pills=True
+            )],
+            style=self.sidebar_style
+        )
+
+        self.mainHome = html.Div(id="main-page", style=self.mainHomeStyle, children=[
             html.H1(
-                children='Hello General Kenobi',
+                'Hello General Kenobi',
                 style={
-                        'textAlign': 'center',
-                        'color': self.colors['text']
+                    'textAlign': 'center',
+                    'color': self.colors['text']
                 }
             ),
             html.Div(
-                children='Dash: A weeb application framework for Pythoon.',
+                'Dash: A weeb application framework for Pythoon.',
                 style={
                     'textAlign': 'center',
                     'color': self.colors['text']
@@ -38,25 +69,30 @@ class MainPage:
             dcc.Graph(
                 id='Avg-age-group',
                 figure={
-                    'data': [{'x': self.data2['Age'], 'y': self.data2['size'], 'type': 'bar', 'name': 'Count By groups'}],
-                        'layout': {
-                            'plot_bgcolor': self.colors['background'],
-                            'paper_bgcolor': self.colors['background'],
-                            'font': {
-                                'color': self.colors['text']
-                            }
+                    'data': [
+                        {'x': self.data2['Age'], 'y': self.data2['size'], 'type': 'bar', 'name': 'Count By groups'}],
+                    'layout': {
+                        'plot_bgcolor': self.colors['background'],
+                        'paper_bgcolor': self.colors['background'],
+                        'font': {
+                            'color': self.colors['text']
                         }
                     }
-                ),
-            dcc.Dropdown(
-                id='Age-Group-Drop',
-                options=self.dropDownDict,
-                placeholder='Select an age group',
-                clearable=False,
-                searchable=False
-
+                }
             )
-            ])
+            # dcc.Dropdown(
+            #     id='Age-Group-Drop',
+            #     options=self.dropDownDict,
+            #     placeholder='Select an age group',
+            #     clearable=False,
+            #     searchable=False
+            #
+            # )
+        ])
+        self.app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+        self.app.callback(Output("main-page", "children"), [Input("url", "pathname")])(self.renderPage)
+        self.app.layout = html.Div([dcc.Location(id="url"), self.sidebar, self.mainHome])
+
 
     # @app.callback(
     #     dash.dependencies.Output('Avg-age-group', 'figure'),
@@ -69,3 +105,14 @@ class MainPage:
     def runPage(self):
         self.app.run_server(debug=True)
 
+
+    def renderPage(self, pathname):
+        if pathname == "/":
+            return self.mainHome
+        elif pathname == "/Analytics":
+            return html.P("YOU THOUGHT THIS WAS ANALYTICS, IT WAS I CYBER DIO!!!!!!", style={"color": 'white'})
+        return dbc.Jumbotron([
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr,
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ])
